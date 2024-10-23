049a6cf4269e5a1db2cde1c051ff900e5394eb63
AFSD-Cazila-Denisa-Maria <cazila.ni.denisa@student.utcluj.ro>
2024-10-23 13:57:50 +0300

    tema2

--- lab3.py
+++ lab3.py
@@ -1,8 +1,19 @@
 import string
-sir = ""
+articol =""""
 Autoritățile au anunțat măsuri suplimentare pentru prevenirea răspândirii virusului.
 Aceasta include noi restricții de circulație și recomandări stricte privind distanțarea socială.
-""
-mid = len(sir) // 2
-prima_parte = sir[:mid]
-a_doua_parte = sir[mid:]
+"""
+# 1
+mijloc = len(articol) // 2
+prima_parte = articol[:mijloc]
+a_doua_parte = articol[mijloc:]
+# Procesarea primei părți:
+prima_parte = prima_parte.upper().strip()
+
+# Procesarea celei de-a doua părți:
+a_doua_parte = a_doua_parte[::-1]
+a_doua_parte = a_doua_parte.capitalize()
+a_doua_parte = ''.join([char for char in a_doua_parte if char not in string.punctuation])
+# 2
+rezultat_final = prima_parte + " " + a_doua_parte
+print(rezultat_final)
