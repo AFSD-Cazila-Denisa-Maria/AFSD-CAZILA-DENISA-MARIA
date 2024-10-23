4618654880d08ec4eec8249a15abe2ba418708b7
AFSD-Cazila-Denisa-Maria <cazila.ni.denisa@student.utcluj.ro>
2024-10-23 13:46:50 +0300

    tema2

---
+++ lab3.py
@@ -0,0 +1,8 @@
+import string
+sir = ""
+Autoritățile au anunțat măsuri suplimentare pentru prevenirea răspândirii virusului.
+Aceasta include noi restricții de circulație și recomandări stricte privind distanțarea socială.
+""
+mid = len(sir) // 2
+prima_parte = sir[:mid]
+a_doua_parte = sir[mid:]
